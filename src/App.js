5b82ec5345a28369a2501e683fe0d7aaceaced95
tester <aiforeveryoneai@gmail.com>
2020-07-19 17:48:01 +0500

    Update App.js
    Added moving effect to the ball

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
 import "./App.css";
 import Dog from "./images/dog.gif";
 import Ball from "./images/ball.gif";
 
 function App() {
+  const { ref } = useWebAnimations({
+    keyframes: [
+      { transform: "translate(0, 0)" },
+      { transform: "translate(-1400px,0)" },
+    ],
+    timing: {
+      duration: 3000,
+      iterations: Infinity,
+      direction: "normal",
+    },
+  });
+
   return (
     <div>
-      <img className="ball" src={Ball} alt="ball" />
-      <img className="dog" src={Dog} alt="" />
+      <img ref={ref} className="ball" src={Ball} alt="ball" />
+      <img className="dog" src={Dog} alt="dog" />
     </div>
   );
 }
